1e1a0f0ddfa0ac329765261331ef6f6ee4a8c0c7
Devin Beliveau <devinmarieb@gmail.com>
2017-03-14 12:19:27 -0600

    Change / to *, add app.use

--- src/app.js
+++ src/app.js
@@ -11,6 +11,16 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/bower_components', express.static(__dirname + '/../bower_components'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/partials', express.static(__dirname + '/partials'));
+
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', { root: __dirname });
+});
+
 if (process.env.NODE_ENV !== 'production') {
   const webpack = require('webpack');
   const webpackDevMiddleware = require('webpack-dev-middleware');
@@ -30,7 +40,7 @@ app.use('/assets', express.static(path.join(__dirname, '../app/assets')));
 app.get('/', function (req, res) { res.sendFile(path.join(__dirname, '/../index.html')) });
 
 app.use('/api', users);
-app.get('/', function (req, res) { res.sendFile(path.join(__dirname, '/../index.html')) });
+app.get('*', function (req, res) { res.sendFile(path.join(__dirname, '/../index.html')) });
 
 app.listen(port);
 
