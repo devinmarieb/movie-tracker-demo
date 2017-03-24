78edca6f9c30b9cbcfcb079b2d8ed88d7008777d
Devin Beliveau <devinmarieb@gmail.com>
2017-03-24 12:09:51 -0600

    Change css to public

--- src/app.js
+++ src/app.js
@@ -11,11 +11,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/bower_components', express.static(__dirname + '/../bower_components'));
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/partials', express.static(__dirname + '/partials'));
-
 app.all('/*', function(req, res, next) {
     // Just send the index.html for other files to support HTML5Mode
     res.sendFile('index.html', { root: __dirname });
